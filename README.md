9938994a77941841421003d5e95290fb6934fc09
Prajakta <prajakta@prajakta.(none)>
2013-07-04 14:43:32 +0530

    [TASK] Implemented light box and extended page properties in backend for Lelesys.Plugin.SlideShow
    Removed white spaces from NodeTypes.yaml
    Change-Id: Icde79f4e596dd25d551f21703fa49affb643f8b5

--- README.md
+++ README.md
@@ -18,6 +18,10 @@ Include the plugin's Stylesheet to your own one's where you add other stylesheet
 
 ``<link href="{f:uri.resource(path: 'resource://Lelesys.Plugin.SlideShow/Public/Stylesheets/slideShow.css')}" rel="stylesheet" media="screen">
 ``
+
+``<link href="{f:uri.resource(path: 'resource://Lelesys.Plugin.SlideShow/Public/Stylesheets/jquery.lightbox-0.5.css')}" rel="stylesheet" media="screen">
+``
+
 Include the plugin's Javascript to your own one's where you add other javascript of the site.
 
 ``	<script src="{f:uri.resource(path: 'resource://Lelesys.Plugin.SlideShow/Public/JavaScript/bootstrap-carousel.js')}"></script>
@@ -29,9 +33,14 @@ Include the plugin's Javascript to your own one's where you add other javascript
 ``	<script src="{f:uri.resource(path: 'resource://Lelesys.Plugin.SlideShow/Public/JavaScript/slideShow.js')}"></script>
 ``
 
+``	<script src="{f:uri.resource(path: 'resource://Lelesys.Plugin.SlideShow/Public/JavaScript/jquery.lightbox-0.5.js')}"></script>
+``
+
 Usage
 -----
 Add the plugin content element "Lelesys Slideshow Container" to the position of your choice.
 This will add container to add all the slides.
 Add the plugin content element "Lelesys Slideshow Item" under "Lelesys Slideshow Container".
 Add images which you want in slideshow. You will see added images in one tab in thumbnailview and in other tab you will see slideshow view.
+When you select slide shoe container you will some extended page properties at the left.
+With this option you can make changes like controls for navigation, slide interval time, gallery size and light box.
--- Resources/Public/JavaScript/slideShow.js
+++ Resources/Public/JavaScript/slideShow.js
@@ -1,13 +1,46 @@
 /*Initialized the carousel and to add active class for the slide item*/
 jQuery(document).ready(function() {
 	jQuery('.carousel-inner .item:first-child').addClass('active');
-	jQuery('.carousel').carousel({
-		interval: 2000
-	})
+
+	// js for slider + jquery.tinycarousel.min.js is required for slider
+	jQuery('.carousel').each(function() {
+		var properties = $.parseJSON($(this).find('span.properties').attr('data-type'));
+		jQuery(this).carousel({
+			interval: parseInt(properties['intervaltime'])
+		});
+		if (parseInt(properties['gallerywidth'])) {
+			var width = parseInt(properties['gallerywidth']);
+			var parentWidth = jQuery('.slideshow-wrapper').parent().width();
+			var percent = Math.round(100 * width / parentWidth);
+			jQuery('.slideshow-wrapper').width(percent + '%');
+		}
+		if (!properties['controls']) {
+			jQuery(this).children('a.carousel-control').addClass('hide');
+		}
+		if (properties['lightBox']) {
+			//This function is for light box
+			jQuery('.carousel-inner a').lightBox({
+				imageLoading: '/_Resources/Static/Packages/Lelesys.Plugin.SlideShow/Images/lightbox-ico-loading.gif',
+				imageBtnPrev: '/_Resources/Static/Packages/Lelesys.Plugin.SlideShow/Images/lightbox-btn-prev.gif',
+				imageBtnNext: '/_Resources/Static/Packages/Lelesys.Plugin.SlideShow/Images/lightbox-btn-next.gif',
+				imageBtnClose: '/_Resources/Static/Packages/Lelesys.Plugin.SlideShow/Images/lightbox-btn-close.gif',
+				imageBlank: '/_Resources/Static/Packages/Lelesys.Plugin.SlideShow/Images/lightbox-blank.gif'
+			});
+		}
+
+
+	});
+// end of js for slider
+
+	//This function is for Tab structure
 	jQuery('#myTab a').click(function(e) {
 		e.preventDefault();
 		jQuery(this).tab('show');
-	})
+	});
+
+
+
+
 });
 
 
